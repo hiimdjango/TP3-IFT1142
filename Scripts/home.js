f42dea025aa50a3b80e2e5f77617bd4848f5edcc
Ahmed <ahmedbendaya@outlook.com>
2018-12-07 18:32:29 -0500

    Update

--- Scripts/home.js
+++ Scripts/home.js
@@ -1,1 +1,11 @@
-console.log('test')
+//Charger le fichier XML
+if (window.XMLHttpRequest)  
+    xhttp=new XMLHttpRequest();
+    else // IE 5/6
+    xhttp=new ActiveXObject("Microsoft.XMLHTTP");
+xhttp.open("GET","../produits.xml",false);
+xhttp.send();
+
+var xmlDoc = xhttp.responseXML; 
+var root = xmlDoc.documentElement;
+
