6f0f93907b81fba83bd5900e929d4fab1d664082
Ahmed <ahmedbendaya@outlook.com>
2018-12-07 19:30:27 -0500

    quick Update

--- Scripts/home.js
+++ Scripts/home.js
@@ -1,11 +1,24 @@
 //Charger le fichier XML
 if (window.XMLHttpRequest)  
     xhttp=new XMLHttpRequest();
-    else // IE 5/6
+else // IE 5/6
     xhttp=new ActiveXObject("Microsoft.XMLHTTP");
+
 xhttp.open("GET","../produits.xml",false);
 xhttp.send();
 
 var xmlDoc = xhttp.responseXML; 
 var root = xmlDoc.documentElement;
 
+//Fonction pour afficher la date et l'heure
+function displayDate() {
+    d = new Date();
+    str ="";
+    var jours = ["Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi","Dimanche"];
+    var mois = ["Janvier","Fevrier","Mars","Avril","Mai","Juin","Juillet","Aout","Septembre","Octobre","Novembre","Decembre"];
+    str = jours[d]
+    document.getElementById('date').innerHTML = jours[d.getDay()] + ", " + d.getDate() + " " + mois[d.getMonth()] + " " + d.getFullYear() + "&nbsp;&nbsp;" + d.getHours() + "h" + d.getMinutes();
+}
+//Actualiser l'heure
+setInterval(displayDate,60000);
+document.onload = displayDate();
